773fa2e2a717dd915d59fd89630294bb8d651ae8
ManojKartheek <1gamerjhonrambo@gmail.com>
2024-12-08 19:38:34 +0530

    changed media_root

--- my_site/settings.py
+++ my_site/settings.py
@@ -137,5 +137,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = BASE_DIR / "uploads"
+# MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_ROOT = "/uploads"
 MEDIA_URL = "/post-files/"
